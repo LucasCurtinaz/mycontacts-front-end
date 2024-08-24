6570090971932a403ffa5f194eaca7e58cb6b2e6
LucasCurtinaz <lucascurtinazlc@gmail.com>
2024-08-23 22:15:31 -0300

    first commit

---
+++ src/components/ContactForm/index.js
@@ -0,0 +1,75 @@
+import { PropTypes } from 'prop-types';
+import { useState } from 'react';
+
+import { ButtonContainer, Form } from './styles';
+
+import Button from '../Button';
+import FormGroup from '../FormGroup';
+import Input from '../Input';
+import Select from '../Select';
+
+export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({
+      name,
+      email,
+      phone,
+      category,
+    });
+  }
+
+  return (
+    <Form onSubmit={(event) => handleSubmit(event)}>
+      <FormGroup>
+        <Input
+          placeholder="Nome"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Input
+          placeholder="E-mail"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Categoria</option>
+          <option value="instagram">Instagram</option>
+          <option value="discord">Discord</option>
+        </Select>
+      </FormGroup>
+
+      <ButtonContainer>
+        <Button type="submit">
+          {buttonLabel}
+        </Button>
+      </ButtonContainer>
+    </Form>
+  );
+}
+
+ContactForm.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+};
