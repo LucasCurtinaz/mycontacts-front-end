4b886d4a2b01f4a18e29a421a8e42cd2189e495b
LucasCurtinaz <lucascurtinazlc@gmail.com>
2024-10-22 23:01:49 -0300

    feat: toast

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,6 +7,7 @@ import defaultTheme from '../../assets/styles/themes/default';
 import Routes from '../../Routes';
 import Header from '../Header';
 
+import ToastContainer from '../Toast/ToastContainer';
 import { Container } from './styles';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
     <BrowserRouter>
       <ThemeProvider theme={defaultTheme}>
         <GlobalStyles />
+        <ToastContainer />
 
         <Container>
           <Header />
