e2d3bb44eccc43ea5f8e6a4f847985cc9842c54f
LucasCurtinaz <lucascurtinazlc@gmail.com>
2024-10-22 23:16:11 -0300

    feat: toast state

--- src/components/Toast/ToastContainer/index.js
+++ src/components/Toast/ToastContainer/index.js
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import ToastMessage from '../ToastMessage';
 import { Container } from './styles';
 
 export default function ToastContainer() {
+  const [messages] = useState([
+    { id: Math.random(), type: 'default', text: 'Default text' },
+    { id: Math.random(), type: 'danger', text: 'Danger text' },
+    { id: Math.random(), type: 'success', text: 'Success text' },
+  ]);
+
   return (
     <Container>
-      <ToastMessage text="Default toast" />
-      <ToastMessage text="Error toast" type="danger" />
-      <ToastMessage text="Success toast" type="success" />
+      {messages.map((message) => (
+        <ToastMessage
+          key={message.id}
+          type={message.type}
+          text={message.text}
+        />
+      ))}
     </Container>
   );
 }
