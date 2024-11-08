4f7e1e53dd91a80b1c59f6dce0e210f6c3bbfab4
LucasCurtinaz <lucascurtinazlc@gmail.com>
2024-11-07 21:17:04 -0300

    feat: animate modal exit

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,4 +1,5 @@
 import { PropTypes } from 'prop-types';
+import { useEffect, useState } from 'react';
 
 import { Container, Footer, Overlay } from './styles';
 
@@ -16,14 +17,33 @@ export default function Modal({
   onCancel,
   onConfirm,
 }) {
-  if (!visible) {
+  const [shouldRender, setShouldRender] = useState(visible);
+
+  useEffect(() => {
+    if (visible) {
+      setShouldRender(true);
+    }
+
+    let timeoutId;
+    if (!visible) {
+      timeoutId = setTimeout(() => {
+        setShouldRender(false);
+      }, 200);
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [visible]);
+
+  if (!shouldRender) {
     return null;
   }
 
   return (
     <ReactPortal containerId="modal-root">
-      <Overlay>
-        <Container danger={danger}>
+      <Overlay isLeaving={!visible}>
+        <Container danger={danger} isLeaving={!visible}>
           <h1>{title}</h1>
 
           <div className="modal-body">
