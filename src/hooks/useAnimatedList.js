dcb055309309cba831322bbba6898494c271d566
LucasCurtinaz <lucascurtinazlc@gmail.com>
2024-11-12 22:21:13 -0300

    refactor: useAnimatedList animation end

--- src/hooks/useAnimatedList.js
+++ src/hooks/useAnimatedList.js
@@ -1,8 +1,12 @@
-import { useCallback, useState } from 'react';
+import {
+  createRef, useCallback, useRef, useState,
+} from 'react';
 
 export default function useAnimatedList(initialValue = []) {
   const [items, setItems] = useState(initialValue);
   const [pendingRemovalItemsIds, setPendingRemovalItemsIds] = useState([]);
+
+  const animatedRefs = useRef(new Map());
 
   const handleRemoveItem = useCallback((id) => {
     setPendingRemovalItemsIds(
@@ -10,24 +14,39 @@ export default function useAnimatedList(initialValue = []) {
     );
   }, []);
 
-  const handleAnimationEnd = useCallback((id) => {
-    setItems((prevState) => prevState.filter((item) => item.id !== id));
-    setPendingRemovalItemsIds(
-      (prevState) => prevState.filter((itemId) => itemId !== id),
-    );
+  // const handleAnimationEnd = useCallback((id) => {
+  //   setItems((prevState) => prevState.filter((item) => item.id !== id));
+  //   setPendingRemovalItemsIds(
+  //     (prevState) => prevState.filter((itemId) => itemId !== id),
+  //   );
+  // }, []);
+
+  const getAnimatedRef = useCallback((itemId) => {
+    let animatedRef = animatedRefs.current.get(itemId);
+
+    if (!animatedRef) {
+      animatedRef = createRef();
+      animatedRefs.current.set(itemId, animatedRef);
+    }
+
+    return animatedRef;
   }, []);
 
   const renderList = useCallback((renderItem) => (
-    items.map((item) => renderItem(item, {
-      isLeaving: pendingRemovalItemsIds.includes(item.id),
-    }))
-  ), [items, pendingRemovalItemsIds]);
+    items.map((item) => {
+      const isLeaving = pendingRemovalItemsIds.includes(item.id);
+      const animatedRef = getAnimatedRef(item.id);
+
+      console.log({ animatedRef });
+
+      return renderItem(item, { isLeaving, animatedRef });
+    })
+  ), [items, pendingRemovalItemsIds, getAnimatedRef]);
 
   return {
     items,
     setItems,
     handleRemoveItem,
-    handleAnimationEnd,
     renderList,
   };
 }
